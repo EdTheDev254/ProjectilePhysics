253ab0a357c29adea756d849ace2db54618627a6
Edwin Wagha <edwin.wagha20@gmail.com>
2023-11-20 01:49:37 +0300

    Added Threshold of the graph

--- main.py
+++ main.py
@@ -18,20 +18,7 @@ class Projectile:
         y = self.initial_height + (self.initial_vertical_velocity * time) - (0.5 * self.gravity * time**2)
         return x, y
 
-    def calculate_velocity(self, time):
-        vx = self.initial_horizontal_velocity
-        vy = self.initial_vertical_velocity - self.gravity * time
-        return vx, vy
-
-    def calculate_drag_force(self, velocity):
-        drag_force = 0.5 * velocity**2
-        return drag_force
-
-    def calculate_acceleration_due_to_drag(self, velocity):
-        acceleration_due_to_drag = self.calculate_drag_force(velocity)
-        return acceleration_due_to_drag
-
-def plot_trajectory_realtime(projectile, max_time):
+def plot_trajectory_realtime(projectile, max_height_threshold, max_range_threshold):
     plt.style.use('dark_background')  # Set background to black
 
     plt.ion()
@@ -41,9 +28,8 @@ def plot_trajectory_realtime(projectile, max_time):
     ax.set_xlabel('Horizontal Distance (m)')
     ax.set_ylabel('Vertical Distance (m)')
 
-    #ax.set_xlim(0, 2000)
-    ax.set_xlim(0, max_time * projectile.initial_horizontal_velocity)
-    ax.set_ylim(0, projectile.initial_height + 1000)
+    ax.set_xlim(0, max_range_threshold)  # Initial x-axis limit
+    ax.set_ylim(0, max_height_threshold)  # Initial y-axis limit
 
     line, = ax.plot([], [], label=f"Speed: {projectile.initial_speed} m/s\nAngle: {math.degrees(projectile.launch_angle)}°\nHeight: {projectile.initial_height} m", color='orange')  # Bright color
 
@@ -53,28 +39,33 @@ def plot_trajectory_realtime(projectile, max_time):
 
     time = 0
     max_height = 0
+    max_range = 0
     try:
-        while time <= max_time:
+        while True:
             x, y = projectile.calculate_position(time)
+
+            # Break the loop if the projectile hits the ground
             if y < 0:
                 break
 
             line.set_xdata(list(line.get_xdata()) + [x])
             line.set_ydata(list(line.get_ydata()) + [y])
 
+            # Update max_height and max_range
+            if y > max_height:
+                max_height = y
+            if x > max_range:
+                max_range = x
+
             plt.draw()
             plt.pause(0.01)
 
-            vx, vy = projectile.calculate_velocity(time)
-            velocity = math.sqrt(vx**2 + vy**2)
-            acceleration_due_to_drag = projectile.calculate_acceleration_due_to_drag(velocity)
             time += 0.05
-
-            if y > max_height:
-                max_height = y
 
     except KeyboardInterrupt:
         pass  # Catch KeyboardInterrupt when the user closes the plot window
+
+    # ... (previous code)
 
     range_value = x
     # Second legend (max height and range) is placed below the first one at the top right
@@ -88,11 +79,14 @@ def main():
     initial_speed = float(input("Enter initial speed (m/s): "))
     launch_angle = float(input("Enter launch angle (degrees): "))
     initial_height = float(input("Enter initial height (m): "))
+    
+    # Set the threshold values for max height and max range
+    max_height_threshold = float(input("Enter the maximum height threshold (m): "))
+    max_range_threshold = float(input("Enter the maximum range threshold (m): "))
 
     projectile = Projectile(initial_speed, launch_angle, initial_height)
 
-    max_simulation_time = 30
-    plot_trajectory_realtime(projectile, max_simulation_time)
+    plot_trajectory_realtime(projectile, max_height_threshold, max_range_threshold)
 
 if __name__ == "__main__":
     main()
