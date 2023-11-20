af164107cd7a16e0015092e6b96159c01466f7a0
Edwin Wagha <edwin.wagha20@gmail.com>
2023-11-20 03:08:55 +0300

    More functionality Updates/scaling axes correctly

--- main.py
+++ main.py
@@ -18,28 +18,42 @@ class Projectile:
         y = self.initial_height + (self.initial_vertical_velocity * time) - (0.5 * self.gravity * time**2)
         return x, y
 
-def plot_trajectory_realtime(projectile, max_height_threshold, max_range_threshold):
-    plt.style.use('dark_background')  # Set background to black
+    def calculate_max_height(self):
+        return (self.initial_vertical_velocity**2) / (2 * self.gravity)
 
-    plt.ion()
+    def calculate_max_range(self):
+        return (self.initial_speed**2) * math.sin(2 * self.launch_angle) / self.gravity
 
-    fig, ax = plt.subplots()
-    ax.set_title('Projectile Trajectory Simulation')
-    ax.set_xlabel('Horizontal Distance (m)')
-    ax.set_ylabel('Vertical Distance (m)')
+def plot_trajectory_realtime(projectile, ax):
+    ax.set_facecolor('black')  # Set axes background color to black
+    ax.set_title('Projectile Trajectory Simulation', color='white')  # Set title color to white
+    ax.set_xlabel('Horizontal Distance (m)', color='white')  # Set label color to white
+    ax.set_ylabel('Vertical Distance (m)', color='white')  # Set label color to white
 
-    ax.set_xlim(0, max_range_threshold)  # Initial x-axis limit
-    ax.set_ylim(0, max_height_threshold)  # Initial y-axis limit
+    # Set the color of the axes to a bright color
+    ax.spines['bottom'].set_color('white')
+    ax.spines['top'].set_color('white')
+    ax.spines['left'].set_color('white')
+    ax.spines['right'].set_color('white')
+
+    ax.tick_params(axis='x', colors='white')  # Set tick color to white
+    ax.tick_params(axis='y', colors='white')  # Set tick color to white
+
+    # Automatically set the initial x-axis and y-axis limits based on calculated max range and max height
+    max_range = projectile.calculate_max_range()
+    max_height = projectile.calculate_max_height()
+    ax.set_xlim(0, max_range + 1000)  # Adjusted for better visualization
+    ax.set_ylim(0, max_height + 1000)  # Adjusted for better visualization
 
     line, = ax.plot([], [], label=f"Speed: {projectile.initial_speed} m/s\nAngle: {math.degrees(projectile.launch_angle)}°\nHeight: {projectile.initial_height} m", color='orange')  # Bright color
 
     # First legend (details) is fixed at the upper right
-    first_legend = ax.legend(loc='upper right', facecolor='black', edgecolor='white', fontsize='small', labelcolor='white')
+    first_legend = ax.legend(loc='upper right', fontsize='small', facecolor='black', edgecolor='white', labelcolor='white')
     ax.add_artist(first_legend)
 
     time = 0
-    max_height = 0
-    max_range = 0
+    max_height_reached = 0
+    max_range_reached = 0
     try:
         while True:
             x, y = projectile.calculate_position(time)
@@ -51,16 +65,16 @@ def plot_trajectory_realtime(projectile, max_height_threshold, max_range_thresho
             line.set_xdata(list(line.get_xdata()) + [x])
             line.set_ydata(list(line.get_ydata()) + [y])
 
-            # Update max_height and max_range
-            if y > max_height:
-                max_height = y
-            if x > max_range:
-                max_range = x
+            # Update max_height_reached and max_range_reached
+            if y > max_height_reached:
+                max_height_reached = y
+            if x > max_range_reached:
+                max_range_reached = x
 
             plt.draw()
             plt.pause(0.01)
 
-            time += 0.05
+            time += 0.1
 
     except KeyboardInterrupt:
         pass  # Catch KeyboardInterrupt when the user closes the plot window
@@ -69,24 +83,27 @@ def plot_trajectory_realtime(projectile, max_height_threshold, max_range_thresho
 
     range_value = x
     # Second legend (max height and range) is placed below the first one at the top right
-    second_legend = ax.legend([f"Max Height: {max_height:.2f} m\nRange: {range_value:.2f} m"], loc='upper right', bbox_to_anchor=(1.0, 0.8), facecolor='black', edgecolor='white', fontsize='small', labelcolor='white')
+    second_legend = ax.legend([f"Max Height: {max_height_reached:.2f} m\nRange: {range_value:.2f} m"], loc='upper right', bbox_to_anchor=(1.0, 0.8), fontsize='small', facecolor='black', edgecolor='white', labelcolor='white')
     ax.add_artist(second_legend)
 
-    plt.ioff()
     plt.show()
 
 def main():
     initial_speed = float(input("Enter initial speed (m/s): "))
     launch_angle = float(input("Enter launch angle (degrees): "))
     initial_height = float(input("Enter initial height (m): "))
-    
-    # Set the threshold values for max height and max range
-    max_height_threshold = float(input("Enter the maximum height threshold (m): "))
-    max_range_threshold = float(input("Enter the maximum range threshold (m): "))
 
     projectile = Projectile(initial_speed, launch_angle, initial_height)
 
-    plot_trajectory_realtime(projectile, max_height_threshold, max_range_threshold)
+    # Set the axes limits before starting the simulation
+    max_range = projectile.calculate_max_range()
+    max_height = projectile.calculate_max_height()
+
+    fig, ax = plt.subplots(facecolor='black')  # Set window background color to black
+    ax.set_xlim(0, max_range + 500)  # Adjusted for better visualization
+    ax.set_ylim(0, max_height + 200)  # Adjusted for better visualization
+
+    plot_trajectory_realtime(projectile, ax)
 
 if __name__ == "__main__":
     main()
